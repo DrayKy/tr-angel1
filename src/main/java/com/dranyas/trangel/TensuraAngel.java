a9a32a1ed9bf5a4a8b7b80a5ecd26f98e8692194
DrayKy <48387128+DrayKy@users.noreply.github.com>
2024-12-06 17:58:46 +0100

    t

--- src/main/java/com/dranyas/trangel/TensuraAngel.java
+++ src/main/java/com/dranyas/trangel/TensuraAngel.java
@@ -1,117 +1,166 @@
 package com.dranyas.trangel;
+
+
 import com.dranyas.trangel.config.TensuraAngelConfig;
 import com.dranyas.trangel.registry.TensuraAngelRegistry;
+import com.github.manasmods.tensura.Tensura;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-
+import java.io.InputStream;
+import java.nio.file.CopyOption;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.IConfigSpec;
-import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.config.ModConfig.Type;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.loading.FMLPaths;
+import net.minecraftforge.fml.loading.FileUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
 
 @Mod("trangel")
 public class TensuraAngel {
     public static final String MOD_ID = "trangel";
-    public static final Logger LOGGER = LogManager.getLogger("tensura:angel");
-    @SuppressWarnings("removal")
+    public static final Logger LOGGER = LogManager.getLogger("Tensura:Angels");
+
     public TensuraAngel() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         modEventBus.addListener(this::onCommonSetup);
         TensuraAngelRegistry.register(modEventBus);
-        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, (IConfigSpec) TensuraAngelConfig.SPEC, getConfigFileName("trangel-config"));
-        LOGGER.info("Tensura:Angels");
+        LOGGER.info("AniTen has been loaded!");
+        FileUtils.getOrCreateDirectory(FMLPaths.CONFIGDIR.get().resolve("tensura-reincarnated"), "tensura-reincarnated");
+        ModLoadingContext.get().registerConfig(Type.SERVER, TensuraAngelConfig.SPEC, getConfigFileName("trangel-config"));
+        Path trangel = FileUtils.getOrCreateDirectory(FMLPaths.GAMEDIR.get().resolve("defaultconfigs/tensura-reincarnated"), "tensura-reincarnated");
+        copyDefaultConfig("trangel-config", trangel);
     }
+
     public static Logger getLogger() {
         return LOGGER;
     }
-    private String getConfigFileName(String name) {
-        return String.format("%s/%s.toml", new Object[] { "tensura-reincarnated", name });
-    }
+
     @SubscribeEvent
     public void onCommonSetup(FMLCommonSetupEvent event) {
         LOGGER.info("Common Setup");
-        editTOMLFile();
-        LOGGER.info("Common setup works and TOML file was edited.");
+        if (this.isFirstLaunch()) {
+            this.editTOMLFile();
+            this.leaveATrace();
+            LOGGER.info("Common setup is working, TOML file was edited.");
+        } else {
+            LOGGER.info("Common setup is working, but the TOML file was previously edited.");
+        }
+
+    }
+
+    private boolean isFirstLaunch() {
+        File footprintFile = new File("defaultconfigs/tensura-reincarnated/anitensura_injector_footprint");
+        return !footprintFile.exists();
+    }
+
+    private void leaveATrace() {
+        File footprintFile = new File("defaultconfigs/tensura-reincarnated/anitensura_injector_footprint");
+
+        try {
+            if (footprintFile.createNewFile()) {
+                System.out.println("Anitensura's config injector left a footprint file: " + footprintFile.getAbsolutePath());
+            } else {
+                System.out.println("Anitensura already left a footprint file.");
+            }
+        } catch (IOException var3) {
+            var3.printStackTrace();
+            System.out.println("Error leaving a footprint file: " + var3.getMessage());
+        }
+
     }
 
     public void editTOMLFile() {
         File tomlFile = new File("defaultconfigs/tensura-reincarnated/common.toml");
         StringBuilder contentBuilder = new StringBuilder();
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(tomlFile));
-            try {
-                String line;
-                while ((line = reader.readLine()) != null)
-                    contentBuilder.append(line).append(System.lineSeparator());
-                reader.close();
-            } catch (Throwable throwable) {
-                try {
-                    reader.close();
-                } catch (Throwable throwable1) {
-                    throwable.addSuppressed(throwable1);
-                }
-                throw throwable;
+
+        String line;
+        try (BufferedReader reader = new BufferedReader(new FileReader(tomlFile))) {
+            while((line = reader.readLine()) != null) {
+                contentBuilder.append(line).append(System.lineSeparator());
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.out.println("Error reading the TOML file: " + e.getMessage());
+        } catch (IOException var16) {
+            var16.printStackTrace();
+            System.out.println("Error reading the TOML file: " + var16.getMessage());
             return;
         }
+
         String content = contentBuilder.toString();
-        String[] newStarting = {"trangel:phantom", "trangel:lesser_angel" };
-        String[] newRandom = { "trangel:true_dragon", "trangel:phantom" };
+        line = new String[]{"trangel:phantom", "trangel:lesser_angel"};
+        String[] newRandom = new String[]{"trangel:phantom", "trangel:lesser_angel", "trangel:true_dragon"};
         String startingRacesKey = "startingRaces = [";
         String randomRacesKey = "possibleRandomRaces = [";
-        content = this.addItemsToTOMLList(content, startingRacesKey, newStarting);
+        content = this.addItemsToTOMLList(content, startingRacesKey, line);
         content = this.addItemsToTOMLList(content, randomRacesKey, newRandom);
-        try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter(tomlFile));
-            try {
-                writer.write(content);
-                writer.close();
-            } catch (Throwable throwable) {
-                try {
-                    writer.close();
-                } catch (Throwable throwable1) {
-                    throwable.addSuppressed(throwable1);
-                }
-                throw throwable;
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.out.println("Error writing to the TOML file: " + e.getMessage());
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(tomlFile))) {
+            writer.write(content);
+        } catch (IOException var14) {
+            var14.printStackTrace();
+            System.out.println("Error writing to the TOML file: " + var14.getMessage());
         }
+
         System.out.println("Items added to TOML lists successfully.");
     }
+
+
     private String addItemsToTOMLList(String content, String listKey, String[] newItems) {
         int index = content.indexOf(listKey);
         if (index == -1) {
             System.out.println("List identifier '" + listKey + "' not found.");
             return content;
+        } else {
+            int endIndex = content.indexOf("]", index) + 1;
+            if (endIndex == 0) {
+                System.out.println("Closing bracket not found for list: " + listKey);
+                return content;
+            } else {
+                String listContent = content.substring(index, endIndex);
+
+                for(String newItem : newItems) {
+                    if (!listContent.contains(newItem)) {
+                        listContent = listContent.replace("]", ", \"" + newItem + "\"]");
+                    }
+                }
+
+                return content.replace(content.substring(index, endIndex), listContent);
+            }
         }
-        int endIndex = content.indexOf("]", index) + 1;
-        if (endIndex == 0) {
-            System.out.println("Closing bracket not found for list: " + listKey);
-            return content;
-        }
-        String listContent = content.substring(index, endIndex);
-        for (String newItem : newItems) {
-            if (!listContent.contains(newItem))
-                listContent = listContent.replace("]", ", \"" + newItem + "\"]");
-        }
-        return content.replace(content.substring(index, endIndex), listContent);
     }
 
+    public static void copyDefaultConfig(String path, Path destination) {
+        String resourcePath = String.format("%s/%s/%s.toml", "defaultconfigs", "tensura-reincarnated", path);
 
+        try (InputStream inputStream = Tensura.class.getClassLoader().getResourceAsStream(resourcePath)) {
+            if (inputStream == null) {
+                throw new IllegalArgumentException("Resource not found: " + resourcePath);
+            }
+
+            if (Files.exists(FMLPaths.GAMEDIR.get().resolve("defaultconfigs/tensura-reincarnated/trangel-config"), new LinkOption[0])) {
+                throw new IllegalArgumentException("No need, file already exist: " + resourcePath);
+            }
+
+            Path replace = FileUtils.getOrCreateDirectory(FMLPaths.GAMEDIR.get().resolve("defaultconfigs/" + getConfigFileName(path)), "tensura-reincarnated");
+            Files.copy(inputStream, replace, new CopyOption[]{StandardCopyOption.REPLACE_EXISTING});
+        } catch (Exception var8) {
+            var8.printStackTrace();
+        }
+
+    }
+
+    private static String getConfigFileName(String name) {
+        return String.format("%s/%s.toml", "tensura-reincarnated", name);
+    }
 }
